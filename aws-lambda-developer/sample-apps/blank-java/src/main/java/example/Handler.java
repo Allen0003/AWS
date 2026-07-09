e9a38556686ce0f70cc7b3f14fbf5261278e5dce
Allen0003 <apss1943@gmail.com>
2026-07-09 17:26:51 +0800

    aws init

---
+++ aws-lambda-developer/sample-apps/blank-java/src/main/java/example/Handler.java
@@ -0,0 +1,23 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
+import software.amazon.awssdk.services.lambda.model.LambdaException;
+
+// Handler value: example.Handler
+public class Handler implements RequestHandler<Map<String,String>, String> {
+
+    private static final LambdaClient lambdaClient = LambdaClient.builder().build();
+
+    @Override
+    public String handleRequest(Map<String,String> event, Context context) {
+
+        return event;
+    }
+}
